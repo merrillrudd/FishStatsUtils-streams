9258532f4405acb7e365652f788e7d7fcd725403
merrillrudd <merrillrudd@gmail.com>
2019-01-18 11:24:53 -0800

    setup examples folder

--- R/plot_network.R
+++ R/plot_network.R
@@ -2,9 +2,10 @@
 #' 
 #' \code{plot_network} generates figure showing network nodes, directional arrows, and observations
 #' 
-#' @param Extrapolation_List Output from \code{FishStatsUtils::make_extrapolation_list}
+#' @param Extrapolation_List Output from \code{Prepare_Extrapolation_Data_Fn}
 #' @param Spatial_List Output from \code{FishStatsUtils::make_spatial_list}
 #' @param Data output of VAST::Data_Fn which includes parent_s and child_s
+#' @param Data_Geostat data-frame of data (with columns 'Year', 'Lon', 'Lat' or 'E_km', 'N_km' at a minimum)
 #' @param PlotDir Directory for plots
 #' @param observations default TRUE to include observations on the figure, FALSE to show only network
 #' @param arrows default TRUE to show directional connections between nodes, but FALSE may prevent crash if there are too many points. 
@@ -15,12 +16,30 @@
 
 #' @export
 
-plot_network <- function(Spatial_List, Extrapolation_List, Data, observations=TRUE, arrows=TRUE, root=TRUE, PlotDir=NULL, ...){
+plot_network <- function(Extrapolation_List, Spatial_List, Data, Data_Geostat, observations=TRUE, arrows=TRUE, root=TRUE, PlotDir=NULL, ...){
 
 	## observation locations
-	obs <- data.frame(Extrapolation_List$Data_Extrap)
+	obs1 <- data.frame(Extrapolation_List$Data_Extrap)
+	if(all(Data_Geostat$Lat==obs1$Lat) & all(Data_Geostat$Lon==obs1$Lon)){
+		obs <- cbind.data.frame(Data_Geostat, "E_km"=obs1$E_km, "N_km"=obs1$N_km)
+	} else {
+		if(grepl("E_km", names(Data_Geostat)) & grepl("N_km", names(Data_Geostat))){
+			obs <- Data_Geostat
+		} else{
+			stop("Extrapolation_List and Data_Geostat latitude and longitude locations do not match -- this function requires both")
+		}
+	}
+
 	Network <- cbind.data.frame('parent_s'=Data$parent_s+1, 'child_s'=Data$child_s+1, Spatial_List$loc_x)
 
+	years <- unique(Data_Geostat$Year)[order(unique(Data_Geostat$Year))]
+	Network <- lapply(1:length(years), function(x){
+		out <- cbind.data.frame(Network, "Year"=years[x])
+		return(out)
+	})
+	Network <- do.call(rbind, Network)
+
+	### across years
 	aa <- ggplot(Network) +
 			mytheme()
 
@@ -29,7 +48,7 @@ plot_network <- function(Spatial_List, Extrapolation_List, Data, observations=TR
 
 	## add points and complete figure
 	aa <- aa +
-	    geom_point(aes(x = E_km, y = N_km), color = "black", alpha=0.6) +
+	    geom_point(data = Network %>% filter(Year==years[1]), aes(x = E_km, y = N_km), color = "black", alpha=0.6) +
 	    xlab("Eastings") + ylab("Northings")
 
 	## option to add arrows
@@ -50,6 +69,42 @@ plot_network <- function(Spatial_List, Extrapolation_List, Data, observations=TR
 	## option to add observations
 	if(observations == TRUE) aa <- aa + geom_point(data = obs, aes(x = E_km, y = N_km), color=brewer.pal(3, "Set1")[1], cex=3)
 
-	if(all(is.null(PlotDir))==FALSE) ggsave(file.path(PlotDir, "Network.png"), aa)
-	aa
+	### by year
+	bb <- ggplot(Network) +
+			facet_wrap(~Year) +
+			mytheme() 
+
+	## add roots underneath points
+	if(root == TRUE) bb <- bb + geom_point(data = Network, aes(x = E_km, y = N_km), color="goldenrod", cex=5)
+
+	## add points and complete figure
+	bb <- bb +
+	    geom_point(data = Network, aes(x = E_km, y = N_km), color = "black", alpha=0.6) +
+	    xlab("Eastings") + ylab("Northings")
+
+	## option to add arrows
+	if(arrows == TRUE){
+		l2 <- lapply(1:nrow(Network), function(x){
+			parent <- Network$parent_s[x]
+			find <- Network %>% filter(child_s == parent)
+			if(nrow(find)>0) out <- cbind.data.frame(Network[x,], 'E2'=find$E_km, 'N2'=find$N_km)
+			if(nrow(find)==0) out <- cbind.data.frame(Network[x,], 'E2'=NA, 'N2'=NA)
+			# if(nrow(find)>0) out <- cbind.data.frame(Network[x,], 'long2'=find$long, 'lat2'=find$lat)
+			# if(nrow(find)==0) out <- cbind.data.frame(Network[x,], 'long2'=NA, 'lat2'=NA)
+			return(out)
+		})
+		l2 <- do.call(rbind, l2)
+		bb <- bb + geom_segment(data=l2, aes(x = E2,y = N2, xend = E_km, yend = N_km), arrow=arrow(length=unit(0.20,"cm"), ends="first", type = "closed"), col="gray", alpha=0.6)
+	}
+
+	## option to add observations
+	if(observations == TRUE) bb <- bb + geom_point(data = obs, aes(x = E_km, y = N_km), color=brewer.pal(3, "Set1")[1], cex=3)
+
+	if(all(is.null(PlotDir))==FALSE){
+		ggsave(file.path(PlotDir, "Network.png"), aa)
+		ggsave(file.path(PlotDir, "Network_byYear.png"), aa)
+	}
+	print(aa)
+	dev.new()
+	print(bb)
 }
---
+++ examples/NZ_models.R
@@ -0,0 +1,305 @@
+rm(list=ls())
+
+#################
+## Packages
+#################
+
+devtools::install_github("james-thorson/VAST", ref="development")
+devtools::install_github("merrillrudd/FishStatsUtils")
+devtools::install_github("merrillrudd/RuddR")
+devtools::install_github("merrillrudd/StreamUtils")
+
+library(VAST)
+library(FishStatsUtils)
+library(StreamUtils)
+library(TMB)
+library(tidyverse)
+library(RColorBrewer)
+library(proj4)
+library(RuddR)
+
+#########################
+## read in data
+#########################
+
+data <- data("nz_waitaki_longfin_eel", package="StreamUtils")
+
+network <- nz_waitaki_longfin_eel[["network"]]
+obs <- nz_waitaki_longfin_eel[["observations"]]
+
+Network_sz = network %>% select( -c("long","lat") )
+
+##################################
+## model - encounter observations
+##################################
+
+##### General settings
+Version = "VAST_v5_3_0" # SpatialDeltaGLMM::get_latest_version( package="VAST" )
+Method = "Stream_network"
+grid_size_km = 1
+n_x = nrow(network)   # Specify number of stations (a.k.a. "knots")
+strata.limits <- data.frame('STRATA'="All_areas")
+
+##### add small value to encounter observations
+present <- obs$present
+devs <- rnorm(length(present), 0, 0.01)
+present_new <- sapply(1:length(present), function(x) ifelse(present[x]==1, present[x]+devs[x], present[x]))
+obs$present <- present_new
+
+##### setup data frame
+Data_Geostat <- data.frame( "Catch_KG" = present_new, 
+              "Year" = obs$year,
+               "Vessel" = "missing", 
+               "AreaSwept_km2" = 1, 
+               "Lat" = obs$lat, 
+               "Lon" = obs$long, 
+               "Pass" = 0)
+
+
+## include latitude and longitude for user-supplied area
+Extrapolation_List = FishStatsUtils::make_extrapolation_info( Region="User", 
+  input_grid=cbind("Lat"=obs$lat, 
+                    "Lon"=obs$long,
+                    "Area_km2"=1), 
+                  strata.limits=strata.limits )
+
+## change latitude and longitude by node, not using Kmeans
+Spatial_List = FishStatsUtils::make_spatial_info( n_x=n_x, 
+                          Method=Method, 
+                          Lon_i=Data_Geostat[,'Lon'], 
+                          Lat_i=Data_Geostat[,'Lat'], 
+                          "LAT_intensity"=network$lat, 
+                          "LON_intensity"=network$long, 
+                          Extrapolation_List=Extrapolation_List, 
+                          Save_Results=TRUE )
+
+## add locations to dataset
+Data_Geostat = cbind( Data_Geostat, "knot_i"=Spatial_List$knot_i )
+
+########################
+## No spatial variation
+########################
+FieldConfig = c("Omega1"=0, "Epsilon1"=0, "Omega2"=0, "Epsilon2"=0)
+RhoConfig = c("Beta1"=2, "Beta2"=3, "Epsilon1"=0, "Epsilon2"=0)
+OverdispersionConfig = c("Eta1"=0, "Eta2"=0)
+Options =  c("Calculate_Range"=0, 
+            "Calculate_effective_area"=0)
+ObsModel = c(2,0)
+
+Data = Data_Fn("Version"=Version,
+                  "FieldConfig"=FieldConfig,
+                  "OverdispersionConfig"=OverdispersionConfig,
+                  "RhoConfig"=RhoConfig,
+                  "ObsModel"=ObsModel,
+                  "c_iz"=rep(0,nrow(Data_Geostat)),
+                  "b_i"=Data_Geostat[,'Catch_KG'],
+                  "a_i"=Data_Geostat[,'AreaSwept_km2'],
+                  "v_i"=as.numeric(Data_Geostat[,'Vessel'])-1,
+                  "s_i"=Data_Geostat[,'knot_i']-1,
+                  "t_iz"=Data_Geostat[,'Year'],
+                  "a_xl"=Spatial_List$a_xl,
+                  "MeshList"=Spatial_List$MeshList,
+                  "GridList"=Spatial_List$GridList,
+                  "Method"=Spatial_List$Method,
+                  "Options"=Options,
+                  "Network_sz"=Network_sz,
+                  "CheckForErrors"=FALSE )
+
+plot_network(Spatial_List=Spatial_List, Extrapolation_List=Extrapolation_List, Data=Data, Data_Geostat=Data_Geostat, observations=TRUE, arrows=FALSE, root=FALSE)
+
+TmbList = Build_TMB_Fn("TmbData"=Data, "Version"=Version, "RhoConfig"=RhoConfig, "loc_x"=Spatial_List$loc_x, "Method"=Method)
+Obj = TmbList[["Obj"]]
+
+Opt1 = TMBhelper::Optimize( obj=Obj, lower=TmbList[["Lower"]], upper=TmbList[["Upper"]], getsd=FALSE, bias.correct=TRUE, newtonsteps=0, bias.correct.control=list(sd=TRUE, split=NULL, nsplit=1, vars_to_correct="Index_cyl") )
+check <- TMBhelper::Check_Identifiable(Obj)
+
+## converges without estimating standard error - turn on and estimate
+Opt = TMBhelper::Optimize( obj=Obj, startpar=Opt1$par, lower=TmbList[["Lower"]], upper=TmbList[["Upper"]], getsd=TRUE, bias.correct=TRUE, newtonsteps=3, bias.correct.control=list(sd=TRUE, split=NULL, nsplit=1, vars_to_correct="Index_cyl") )
+
+Report = Obj$report()
+
+## convergence
+Opt$diagnostics[,c('Param','Lower','MLE','Upper','final_gradient')]
+Opt[["SD"]]
+
+## diagnostics for encounter probability component
+Enc_prob = plot_encounter_diagnostic( Report=Report, Data=Data, DirName=NULL)
+
+## diagnostics for positive catch rate component
+Q = plot_quantile_diagnostic( TmbData=Data, Report=Report, FileName_PP="Posterior_Predictive",
+  FileName_Phist="Posterior_Predictive-Histogram", 
+  FileName_QQ="Q-Q_plot", FileName_Qhist="Q-Q_hist")
+
+## Diagnostics for plotting residuals on a map
+# Get region-specific settings for plots
+MapDetails_List = make_map_info( "Region"="User", "NN_Extrap"=Spatial_List$PolygonList$NN_Extrap, "Extrapolation_List"=Extrapolation_List )
+
+# Decide which years to plot                                                   
+Year_Set = seq(min(Data_Geostat[,'Year']),max(Data_Geostat[,'Year']))
+Years2Include = which( Year_Set %in% sort(unique(Data_Geostat[,'Year'])))
+
+## Plot Pearson residuals
+plot_residuals(Lat_i=Data_Geostat[,'Lat'], Lon_i=Data_Geostat[,'Lon'], TmbData=Data, Report=Report, Q=Q,  MappingDetails=MapDetails_List[["MappingDetails"]], PlotDF=MapDetails_List[["PlotDF"]], MapSizeRatio=MapDetails_List[["MapSizeRatio"]], Xlim=MapDetails_List[["Xlim"]], Ylim=MapDetails_List[["Ylim"]], FileName=ModFile, Year_Set=Year_Set, Years2Include=Years2Include, Rotate=MapDetails_List[["Rotate"]], Cex=3, pch=19, Legend=MapDetails_List[["Legend"]], zone=MapDetails_List[["Zone"]], mar=c(0,0,2,0), oma=c(3.5,3.5,0,0))
+
+##### Model output
+## density surface for each year
+Dens_xt = plot_maps(plot_set=c(1,3), MappingDetails=MapDetails_List[["MappingDetails"]], Report=Report, Sdreport=Opt$SD, PlotDF=MapDetails_List[["PlotDF"]], MapSizeRatio=MapDetails_List[["MapSizeRatio"]], Xlim=MapDetails_List[["Xlim"]], Ylim=MapDetails_List[["Ylim"]], Year_Set=Year_Set, Years2Include=Years2Include, Rotate=MapDetails_List[["Rotate"]], Cex=1.5, pch=19, Legend=MapDetails_List[["Legend"]], zone=MapDetails_List[["Zone"]], mar=c(0,0,2,0), oma=c(3.5,3.5,0,0), plot_legend_fig=TRUE)
+
+# ## index of abundance
+Index = plot_biomass_index( TmbData=Data, Sdreport=Opt$SD, Year_Set=Year_Set, Years2Include=Years2Include, use_biascorr=FALSE )
+
+##################
+# I'll try turning on spatial variation
+##################
+space_dir <- file.path(nz_enc_dir, "spatial_temporal_effects")
+dir.create(space_dir)
+setwd(space_dir)
+
+FieldConfig = c("Omega1"=1, "Epsilon1"=0, "Omega2"=0, "Epsilon2"=0)
+RhoConfig = c("Beta1"=2, "Beta2"=3, "Epsilon1"=0, "Epsilon2"=0)
+
+OverdispersionConfig = c("Eta1"=0, "Eta2"=0)
+Options =  c("Calculate_Range"=0,
+            "Calculate_effective_area"=0)
+ObsModel = c(2,0)
+
+Data = Data_Fn("Version"=Version,
+                  "FieldConfig"=FieldConfig,
+                  "OverdispersionConfig"=OverdispersionConfig,
+                  "RhoConfig"=RhoConfig,
+                  "ObsModel"=ObsModel,
+                  "c_iz"=rep(0,nrow(Data_Geostat)),
+                  "b_i"=Data_Geostat[,'Catch_KG'],
+                  "a_i"=Data_Geostat[,'AreaSwept_km2'],
+                  "v_i"=as.numeric(Data_Geostat[,'Vessel'])-1,
+                  "s_i"=Data_Geostat[,'knot_i']-1,
+                  "t_iz"=Data_Geostat[,'Year'],
+                  "a_xl"=Spatial_List$a_xl,
+                  "MeshList"=Spatial_List$MeshList,
+                  "GridList"=Spatial_List$GridList,
+                  "Method"=Spatial_List$Method,
+                  "Options"=Options,
+                  "Network_sz"=Network_sz )
+
+TmbList = Build_TMB_Fn("TmbData"=Data, "Version"=Version, "RhoConfig"=RhoConfig, "loc_x"=Spatial_List$loc_x, "Method"=Method)
+Obj = TmbList[["Obj"]]
+Obj$par[grep("logkappa",names(Obj$par))] = log(1/median(Network_sz[,'dist_s']))
+
+Opt1 = TMBhelper::Optimize( obj=Obj, lower=TmbList[["Lower"]], upper=TmbList[["Upper"]], getsd=FALSE, bias.correct=TRUE, newtonsteps=3, bias.correct.control=list(sd=TRUE, split=NULL, nsplit=1, vars_to_correct="Index_cyl") )
+check <- TMBhelper::Check_Identifiable(Obj)
+
+Opt = TMBhelper::Optimize( obj=Obj, lower=TmbList[["Lower"]], upper=TmbList[["Upper"]], getsd=TRUE, bias.correct=TRUE, newtonsteps=3, bias.correct.control=list(sd=TRUE, split=NULL, nsplit=1, vars_to_correct="Index_cyl") )
+check <- TMBhelper::Check_Identifiable(Obj)
+#############
+# It has L_omega1_z going to 0 and gradient for kappa is going to zero, so the model is hitting a bound and wants to turn on spatial variation.  So obviously there's too little informatino here for a spatial model.
+Opt$diagnostics[,c('Param','Lower','MLE','Upper','final_gradient')]
+
+Opt[["SD"]]
+
+Report <- Obj$report()
+
+plot_data(Extrapolation_List=Extrapolation_List, Spatial_List=Spatial_List, Data_Geostat=Data_Geostat, pch=19, cex=3 )
+
+## diagnostics for encounter probability component
+Enc_prob = plot_encounter_diagnostic( Report=Report, Data=Data)
+
+## diagnostics for positive catch rate component
+Q = plot_quantile_diagnostic( TmbData=Data, Report=Report, FileName_PP="Posterior_Predictive",
+  FileName_Phist="Posterior_Predictive-Histogram", 
+  FileName_QQ="Q-Q_plot", FileName_Qhist="Q-Q_hist")
+
+## Diagnostics for plotting residuals on a map
+# Get region-specific settings for plots
+MapDetails_List = make_map_info( "Region"="User", "NN_Extrap"=Spatial_List$PolygonList$NN_Extrap, "Extrapolation_List"=Extrapolation_List )
+
+# Decide which years to plot                                                   
+Year_Set = seq(min(Data_Geostat[,'Year']),max(Data_Geostat[,'Year']))
+Years2Include = which( Year_Set %in% sort(unique(Data_Geostat[,'Year'])))
+
+## Plot Pearson residuals
+plot_residuals(Lat_i=Data_Geostat[,'Lat'], Lon_i=Data_Geostat[,'Lon'], TmbData=Data, Report=Report, Q=Q,  MappingDetails=MapDetails_List[["MappingDetails"]], PlotDF=MapDetails_List[["PlotDF"]], MapSizeRatio=MapDetails_List[["MapSizeRatio"]], Xlim=MapDetails_List[["Xlim"]], Ylim=MapDetails_List[["Ylim"]], FileName=ModFile, Year_Set=Year_Set, Years2Include=Years2Include, Rotate=MapDetails_List[["Rotate"]], Cex=3, pch=19, Legend=MapDetails_List[["Legend"]], zone=MapDetails_List[["Zone"]], mar=c(0,0,2,0), oma=c(3.5,3.5,0,0))
+
+##### Model output
+## density surface for each year
+Dens_xt = plot_maps(plot_set=c(3), MappingDetails=MapDetails_List[["MappingDetails"]], Report=Report, Sdreport=Opt$SD, PlotDF=MapDetails_List[["PlotDF"]], MapSizeRatio=MapDetails_List[["MapSizeRatio"]], Xlim=MapDetails_List[["Xlim"]], Ylim=MapDetails_List[["Ylim"]], Year_Set=Year_Set, Years2Include=Years2Include, Rotate=MapDetails_List[["Rotate"]], Cex=1.5, pch=19, Legend=MapDetails_List[["Legend"]], zone=MapDetails_List[["Zone"]], mar=c(0,0,2,0), oma=c(3.5,3.5,0,0), plot_legend_fig=TRUE)
+
+# ## index of abundance
+Index = plot_biomass_index( TmbData=Data, Sdreport=Opt$SD, Year_Set=Year_Set, Years2Include=Years2Include, use_biascorr=FALSE )
+
+#####################
+## spatiotemporal
+
+space_dir <- file.path(nz_enc_dir, "spatiotemporal_effects")
+dir.create(space_dir)
+setwd(space_dir)
+
+FieldConfig = c("Omega1"=1, "Epsilon1"=1, "Omega2"=0, "Epsilon2"=0)
+RhoConfig = c("Beta1"=2, "Beta2"=3, "Epsilon1"=0, "Epsilon2"=0)
+
+OverdispersionConfig = c("Eta1"=0, "Eta2"=0)
+Options =  c("Calculate_Range"=0,
+            "Calculate_effective_area"=0)
+ObsModel = c(2,0)
+
+Data = Data_Fn("Version"=Version,
+                  "FieldConfig"=FieldConfig,
+                  "OverdispersionConfig"=OverdispersionConfig,
+                  "RhoConfig"=RhoConfig,
+                  "ObsModel"=ObsModel,
+                  "c_iz"=rep(0,nrow(Data_Geostat)),
+                  "b_i"=Data_Geostat[,'Catch_KG'],
+                  "a_i"=Data_Geostat[,'AreaSwept_km2'],
+                  "v_i"=as.numeric(Data_Geostat[,'Vessel'])-1,
+                  "s_i"=Data_Geostat[,'knot_i']-1,
+                  "t_iz"=Data_Geostat[,'Year'],
+                  "a_xl"=Spatial_List$a_xl,
+                  "MeshList"=Spatial_List$MeshList,
+                  "GridList"=Spatial_List$GridList,
+                  "Method"=Spatial_List$Method,
+                  "Options"=Options,
+                  "Network_sz"=Network_sz )
+
+TmbList = Build_TMB_Fn("TmbData"=Data, "Version"=Version, "RhoConfig"=RhoConfig, "loc_x"=Spatial_List$loc_x, "Method"=Method)
+Obj = TmbList[["Obj"]]
+Obj$par[grep("logkappa",names(Obj$par))] = log(1/median(Network_sz[,'dist_s']))
+
+Opt1 = TMBhelper::Optimize( obj=Obj, lower=TmbList[["Lower"]], upper=TmbList[["Upper"]], getsd=FALSE, bias.correct=TRUE, newtonsteps=3, bias.correct.control=list(sd=TRUE, split=NULL, nsplit=1, vars_to_correct="Index_cyl") )
+check <- TMBhelper::Check_Identifiable(Obj)
+
+Opt = TMBhelper::Optimize( obj=Obj, startpar=Opt1$par, lower=TmbList[["Lower"]], upper=TmbList[["Upper"]], getsd=TRUE, bias.correct=TRUE, newtonsteps=3, bias.correct.control=list(sd=TRUE, split=NULL, nsplit=1, vars_to_correct="Index_cyl") )
+check <- TMBhelper::Check_Identifiable(Obj)
+#############
+# It has L_omega1_z going to 0 and gradient for kappa is going to zero, so the model is hitting a bound and wants to turn on spatial variation.  So obviously there's too little informatino here for a spatial model.
+Opt$diagnostics[,c('Param','Lower','MLE','Upper','final_gradient')]
+
+Opt[["SD"]]
+
+Report <- Obj$report()
+
+plot_data(Extrapolation_List=Extrapolation_List, Spatial_List=Spatial_List, Data_Geostat=Data_Geostat, pch=19, cex=3 )
+
+## diagnostics for encounter probability component
+Enc_prob = plot_encounter_diagnostic( Report=Report, Data=Data)
+
+## diagnostics for positive catch rate component
+Q = plot_quantile_diagnostic( TmbData=Data, Report=Report, FileName_PP="Posterior_Predictive",
+  FileName_Phist="Posterior_Predictive-Histogram", 
+  FileName_QQ="Q-Q_plot", FileName_Qhist="Q-Q_hist")
+
+## Diagnostics for plotting residuals on a map
+# Get region-specific settings for plots
+MapDetails_List = make_map_info( "Region"="User", "NN_Extrap"=Spatial_List$PolygonList$NN_Extrap, "Extrapolation_List"=Extrapolation_List )
+
+# Decide which years to plot                                                   
+Year_Set = seq(min(Data_Geostat[,'Year']),max(Data_Geostat[,'Year']))
+Years2Include = which( Year_Set %in% sort(unique(Data_Geostat[,'Year'])))
+
+## Plot Pearson residuals
+plot_residuals(Lat_i=Data_Geostat[,'Lat'], Lon_i=Data_Geostat[,'Lon'], TmbData=Data, Report=Report, Q=Q,  MappingDetails=MapDetails_List[["MappingDetails"]], PlotDF=MapDetails_List[["PlotDF"]], MapSizeRatio=MapDetails_List[["MapSizeRatio"]], Xlim=MapDetails_List[["Xlim"]], Ylim=MapDetails_List[["Ylim"]], FileName=ModFile, Year_Set=Year_Set, Years2Include=Years2Include, Rotate=MapDetails_List[["Rotate"]], Cex=3, pch=19, Legend=MapDetails_List[["Legend"]], zone=MapDetails_List[["Zone"]], mar=c(0,0,2,0), oma=c(3.5,3.5,0,0))
+
+##### Model output
+## density surface for each year
+Dens_xt = plot_maps(plot_set=c(3), MappingDetails=MapDetails_List[["MappingDetails"]], Report=Report, Sdreport=Opt$SD, PlotDF=MapDetails_List[["PlotDF"]], MapSizeRatio=MapDetails_List[["MapSizeRatio"]], Xlim=MapDetails_List[["Xlim"]], Ylim=MapDetails_List[["Ylim"]], Year_Set=Year_Set, Years2Include=Years2Include, Rotate=MapDetails_List[["Rotate"]], Cex=1.5, pch=19, Legend=MapDetails_List[["Legend"]], zone=MapDetails_List[["Zone"]], mar=c(0,0,2,0), oma=c(3.5,3.5,0,0), plot_legend_fig=TRUE)
+
+# ## index of abundance
+Index = plot_biomass_index( TmbData=Data, Sdreport=Opt$SD, Year_Set=Year_Set, Years2Include=Years2Include, use_biascorr=FALSE )
